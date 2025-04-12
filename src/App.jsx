571f9cc7cfff045917a1f7425521592ba561f9af
Lokeshwar-Rajan <lokeshwarrajan4@gmail.com>
2025-04-12 20:07:08 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Features from './components/Features/Features'
-import Title from './components/Title/Title'
+import Title from './components/Title/Title';  
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
